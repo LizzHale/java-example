f91fb6eea539ec26bb4b87f2f0baa5721df4fdf3
Lizz Hale <lizzhale@mac.com>
2021-08-05 10:35:40 -0600

    Update example.java

--- example.java
+++ example.java
@@ -2,18 +2,18 @@ public class SortedIntList extends IntListVer3{
     
     public SortedIntList(int initialCap){
         //call IntList constructor
-        super(initialCap);
+		super(initialCap);
     }
     
     public SortedIntList(){
-        super();
+		super();
     }
     
     //override add
     public void add(int value){
         //search for location to insert value
         int pos = 0;
-        while( pos < size() && value > get(pos) ){
+		while( pos < size() && value > get(pos) ){
             pos++;
         }
         super.insert(pos, value);
